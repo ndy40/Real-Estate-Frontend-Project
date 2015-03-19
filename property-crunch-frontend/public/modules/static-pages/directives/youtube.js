6fcdddba0f5cbe9e21520911564406600bc89a35
Arslan Akram <arslanhawn@gmail.com>
2015-03-19 23:46:15 +0500

    - Fixed Youtube video not stopping on modal.close

--- property-crunch-frontend/public/modules/static-pages/directives/youtube.js
+++ property-crunch-frontend/public/modules/static-pages/directives/youtube.js
@@ -14,8 +14,26 @@ define(["../module"], function (app) {
                 
                 scope.loadVideo = function() {
                     scope.status = true;
-                    scope.videoSrc = $sce.trustAsResourceUrl("https://www.youtube.com/embed/i8v_7iD4_pQ?VQ=HD720&amp;rel=0&amp;showinfo=0&autoplay=1");
+                    scope.videoSrc = $sce.trustAsResourceUrl("https://www.youtube.com/embed/i8v_7iD4_pQ?VQ=HD720&amp;rel=0&amp;showinfo=0&autoplay=1&enablejsapi=1");
                 };
+                
+                //http://stackoverflow.com/questions/26539299/disable-youtube-video-in-angularjs-playing-in-bootstrap-modal
+                $('#video').on('hide.bs.modal', function () {
+                    var outerDiv = document.getElementById("video");
+                    var youtubeIframe = outerDiv.getElementsByTagName("iframe")[0].contentWindow;
+                    youtubeIframe.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+                });
+                
+//                $(".modal").click(function(e) {
+//                    if($(e.target).is('img.img-responsive')){
+//                        e.preventDefault();
+//                        return;
+//                    } else {
+//                        var outerDiv = document.getElementById("video");
+//                        var youtubeIframe = outerDiv.getElementsByTagName("iframe")[0].contentWindow;
+//                        youtubeIframe.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+//                    }
+//                }); 
             }
         };
     }]);
