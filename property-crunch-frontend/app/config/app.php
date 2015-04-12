fe794c08f1ecb2411191f82c1a716c1f33e60cfb
Saurabh Singh <saurabhsingh@saurabh-singh.(none)>
2015-04-12 22:17:42 +0530

    Committing code for converting js to html.

--- property-crunch-frontend/app/config/app.php
+++ property-crunch-frontend/app/config/app.php
@@ -122,7 +122,7 @@ return array(
             'Illuminate\View\ViewServiceProvider',
             'Illuminate\Workbench\WorkbenchServiceProvider',
             'Cartalyst\Sentry\SentryServiceProvider',
-
+            'Nutsweb\LaravelPrerender\LaravelPrerenderServiceProvider',
 	),
 
 	/*
