87adbc147730b8c70e27a29df4035f35d21a0d3a
Arslan Akram <arslanhawn@gmail.com>
2015-04-19 15:15:17 +0500

    - Added Start your investment journey on home page - Added Heap Analytics

--- property-crunch-frontend/app/views/index.blade.php
+++ property-crunch-frontend/app/views/index.blade.php
@@ -16,11 +16,19 @@
           <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
         
-        <!-- Google Analytics -->
+        <!-- Start Google Analytics -->
         <script async type="text/javascript">
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-57851758-1', 'auto');ga('send', 'pageview');
         </script>
+        <!-- End Google Analytics -->
         
+        <!-- Start Heap Analytics -->
+        <script async type="text/javascript">
+          window.heap=window.heap||[],heap.load=function(t,e){window.heap.appid=t,window.heap.config=e;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=("https:"===document.location.protocol?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+t+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(t){return function(){heap.push([t].concat(Array.prototype.slice.call(arguments,0)))}},p=["clearEventProperties","identify","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+          heap.load("536553360");
+        </script>
+        <!-- End Heap Analytics -->
+    
         <!--Start of Zopim Live Chat Script-->
         <script async type="text/javascript">
         window.$zopim||(function(d,s){var z=$zopim=function(c){
